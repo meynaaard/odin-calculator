598ec05c08392a18c1c5a04d826914ccc833391b
meynard <sombritomeynard@gmail.com>
2025-07-28 06:52:10 +0800

    feat: Add operate() function
    This change adds an operate() function that calls
    the math functions according to the operator

--- js/script.js
+++ js/script.js
@@ -13,3 +13,28 @@ function multiply(firstNum, secondNum) {
 function divide(firstNum, secondNum) {
   return firstNum / secondNum;
 }
+
+let firstNum;
+let secondNum;
+let operator;
+
+function operate(firstNum, secondNum, operator) {
+  let runningTotal;
+
+  switch (operator) {
+    case "+":
+      runningTotal = add(firstNum, secondNum);
+      break;
+    case "-":
+      runningTotal = subtract(firstNum, secondNum);
+      break;
+    case "*":
+      runningTotal = multiply(firstNum, secondNum);
+      break;
+    case "/":
+      runningTotal = divide(firstNum, secondNum);
+      break;
+  }
+
+  return runningTotal;
+}
