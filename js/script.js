9419836ec08966b51eec69340d37ed764a59e7b7
meynard <sombritomeynard@gmail.com>
2025-07-28 06:23:49 +0800

    feat: Add basic math operators

---
+++ js/script.js
@@ -0,0 +1,15 @@
+function add(firstNum, secondNum) {
+  return firstNum + secondNum;
+}
+
+function subtract(firstNum, secondNum) {
+  return firstNum - secondNum;
+}
+
+function multiply(firstNum, secondNum) {
+  return firstNum * secondNum
+}
+
+function divide(firstNum, secondNum) {
+  return firstNum / secondNum;
+}
