3b4bcd34bd02a0f752c3ebf15d0eb53db8c305fd
meynard <sombritomeynard@gmail.com>
2025-07-28 11:48:22 +0800

    feat: Display clicked button
    This change adds a feature where a clicked button
    is displayed whether a digit or an operator.

--- js/script.js
+++ js/script.js
@@ -38,3 +38,23 @@ function operate(firstNum, secondNum, operator) {
 
   return runningTotal;
 }
+
+const display = document.querySelector('.display');
+const digitButtons = document.querySelectorAll(".calc-buttons > .digit");
+const operatorButtons = document.querySelectorAll(".calc-buttons > .operator");
+
+[...digitButtons, ...operatorButtons].forEach(button => {
+  display.textContent = "";
+  button.addEventListener("click", displayClickedButton);
+});
+
+function displayClickedButton(event) {
+  const target = event.target
+  const buttonValue = target.value;
+
+  if (target.className === "operator") {
+    display.textContent += ` ${buttonValue} `;
+  } else {
+    display.textContent += buttonValue;
+  }
+}
